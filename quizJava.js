b1d08f42d628d8f9b045df1e59aa745005071b58
Wuata <lmgom875@gmail.com>
2019-12-22 21:10:09 -0500

    new files for new Jquery version

--- quizJava.js
+++ quizJava.js
@@ -66,7 +66,6 @@ var answer = document.getElementById("answer");
 var final = document.getElementById("final");
 var btnPrueba = document.getElementById("btnPrueba");
 var timerNumber = document.getElementById("timerNumber");
-var timeRun = "";
 
 //Variables for Question Maker
 
